ebd380f31e81cd185df84a076022a1858267ade8
faizan786007 <kingsmanfaizanali@gmail.com>
2024-09-04 16:08:07 +0530

    udpades

--- test1/src/MovieCard.js
+++ test1/src/MovieCard.js
@@ -2,49 +2,39 @@ import React, { Component } from "react";
 import "./MovieCard.css";
 
 export default class MovieCard extends Component {
-	constructor() {
-		super();
-		this.state = {
-			price: 199,
-			plot: "Movie is about super hero",
-			title: "Avengers",
-			rating: 8.9,
-			star: 0,
-			fav: true,
-		};
-	}
-	addStar = () => {
-		// this.setState({
-		// 	star: this.state.star + 0.5,
-		// });
-		if (this.state.star >= 10) return;
-		this.setState((ps) => {
-			return {
-				star: ps.star + 0.5,
-			};
-		});
-	};
-	decStar = () => {
-		if (this.state.star <= 0) return;
-		this.setState({
-			star: this.state.star - 0.5,
-		});
-	};
-	toggle = () => {
-		this.setState({
-			fav: !this.state.fav,
-		});
-	};
+	// addStar = () => {
+	// 	// this.setState({
+	// 	// 	star: this.state.star + 0.5,
+	// 	// });
+	// 	if (this.state.star >= 10) return;
+	// 	this.setState((ps) => {
+	// 		return {
+	// 			star: ps.star + 0.5,
+	// 		};
+	// 	});
+	// };
+	// decStar = () => {
+	// 	if (this.state.star <= 0) return;
+	// 	this.setState({
+	// 		star: this.state.star - 0.5,
+	// 	});
+	// };
+	// toggle = () => {
+	// 	this.setState({
+	// 		fav: !this.state.fav,
+	// 	});
+	// };
 	render() {
-		const { price, title, plot, rating, star, fav } = this.state;
+		// console.log(this.props.movies);
+		const { movie, addStar, decStar, toggle } = this.props;
+		// console.log(movie);
+		// console.log(addStar);
+		const { price, title, plot, rating, star, fav, src } = movie;
 		return (
 			<div className="main">
 				<div className="card">
 					<div className="left">
-						<img
-							src="https://w0.peakpx.com/wallpaper/551/479/HD-wallpaper-avengers-poster-avengers-poster-marvels-super-hero-superhero-the-avengers.jpg"
-							alt="img"
-						/>
+						<img src={src} alt="img" />
 					</div>
 					<div className="right">
 						<div className="title">{title}</div>
@@ -58,7 +48,7 @@ export default class MovieCard extends Component {
 									className="minus"
 									src="https://cdn-icons-png.flaticon.com/128/43/43625.png"
 									alt=""
-									onClick={this.decStar}
+									onClick={() => decStar(movie)}
 								/>
 								<img
 									className="star-disc"
@@ -69,13 +59,13 @@ export default class MovieCard extends Component {
 									className="plus"
 									src="https://cdn-icons-png.flaticon.com/128/9312/9312231.png"
 									alt=""
-									onClick={this.addStar}
+									onClick={() => addStar(movie)}
 								/>
 								<span>{star}</span>
 							</div>
 							{
 								<button
-									onClick={this.toggle}
+									onClick={() => toggle(movie)}
 									className={fav ? "fav-btn" : "unfav-btn"}
 								>
 									{fav ? "favourite" : "un-favourite"}
--- test1/src/MovieList.js
+++ test1/src/MovieList.js
@@ -2,13 +2,95 @@ import React, { Component } from "react";
 import MovieCard from "./MovieCard";
 
 export default class MovieList extends Component {
+	constructor() {
+		super();
+		this.state = {
+			movies: [
+				{
+					price: 199,
+					plot: "Movie is about super hero",
+					title: "Avengers",
+					rating: 8.9,
+					star: 0,
+					fav: true,
+					src: "https://w0.peakpx.com/wallpaper/551/479/HD-wallpaper-avengers-poster-avengers-poster-marvels-super-hero-superhero-the-avengers.jpg",
+				},
+				{
+					price: 299,
+					plot: "Movie is about iron man hero",
+					title: "Iron Man",
+					rating: 8.0,
+					star: 0,
+					fav: true,
+					src: "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg",
+				},
+				{
+					price: 399,
+					plot: "Movie is about thor hero",
+					title: "Thor",
+					rating: 7.2,
+					star: 0,
+					fav: true,
+					src: "https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg",
+				},
+				{
+					price: 109,
+					plot: "Movie is about Batman hero",
+					title: "Batman",
+					rating: 9.2,
+					star: 0,
+					fav: true,
+					src: "https://image.api.playstation.com/vulcan/img/rnd/202010/2621/H9v5o8vP6RKkQtR77LIGrGDE.png",
+				},
+			],
+		};
+	}
+	handleAddStar = (movie) => {
+		const { movies } = this.state;
+		const mId = movies.indexOf(movie);
+		console.log("mid", mId);
+		if (movies[mId].star >= 5) return;
+		movies[mId].star += 0.5;
+
+		this.setState({
+			movies: movies,
+		});
+	};
+	handleDecStar = (movie) => {
+		const { movies } = this.state;
+		const mId = movies.indexOf(movie);
+		console.log("mid", mId);
+		if (movies[mId].star <= 0) return;
+		movies[mId].star -= 0.5;
+
+		this.setState({
+			movies: movies,
+		});
+	};
+	handleToggle = (movie) => {
+		const { movies } = this.state;
+		let mId = movies.indexOf(movie);
+
+		this.setState({
+			movies: movies.map((m, index) =>
+				index == mId ? { ...m, fav: !m.fav } : m
+			),
+		});
+	};
 	render() {
+		const { movies } = this.state;
 		return (
 			<div>
-				<MovieCard />
-				<MovieCard />
-				<MovieCard />
-				<MovieCard />
+				{/* <MovieCard movies={this.state} /> */}
+				{movies.map((movie, index) => (
+					<MovieCard
+						key={index}
+						movie={movie}
+						addStar={this.handleAddStar}
+						decStar={this.handleDecStar}
+						toggle={this.handleToggle}
+					/>
+				))}
 			</div>
 		);
 	}
